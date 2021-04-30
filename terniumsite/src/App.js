45c9db69fe387cc792b1c9978ffdbea47990a9a0
Valeria Conde <valeriaconde.96@gmail.com>
2021-04-30 15:10:11 -0700

    Test 2

--- terniumsite/src/App.js
+++ terniumsite/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a test
+          This is a test 2.0!
         </p>
         <a
           className="App-link"
