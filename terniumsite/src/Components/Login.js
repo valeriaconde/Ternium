40930ea41dedd866aa5ccacd26a127b04dc17d06
Valeria Conde <valeriaconde.96@gmail.com>
2021-05-13 11:45:22 -0700

    created template

---
+++ terniumsite/src/Components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    /*
+    constructor(props) {
+        super(props);
+    }
+    */
+    render() {
+        return (
+            <div> 
+                <h1> LOGIN PAGE </h1>
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ terniumsite/src/Components/template.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class template extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div> 
+
+            </div>
+        );
+    }
+}
