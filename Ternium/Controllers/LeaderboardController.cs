efc1c43f323d73dd42bde55f6e07fb6cacd5ba60
Valeria Conde <valeriaconde.96@gmail.com>
2021-06-08 19:02:57 -0500

    Both leaderboards work

--- Ternium/Controllers/LeaderboardController.cs
+++ Ternium/Controllers/LeaderboardController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ternium.Models;
+using MySql.Data.MySqlClient;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,13 +13,18 @@ namespace Ternium.Controllers
 {
     public class UserLeaderboard
     {
-        public string Name { get; set; }
-        public int Points { get; set; }
-        public int Rank { get; set; }
+        public int ID { get; set; }
+        public string nombreUsuario { get; set; }
+        public int puntaje { get; set; }
     }
 
     public class LeaderboardController : Controller
     {
+        public IList<UserLeaderboard> ListaLeader { get; set; }
+
+        [BindProperty]
+        public bool ShowJuego { get; set; }
+
         [BindProperty]
         public string TableHTML { get; set; }
 
@@ -28,19 +35,39 @@ namespace Ternium.Controllers
                 return RedirectToAction("Index", "Login");
             }
 
-            List<UserLeaderboard> users = new List<UserLeaderboard>();
+            ListaLeader = new List<UserLeaderboard>();
 
-            users.Add(new UserLeaderboard { Name = "Valeria", Points = 15017, Rank = 1 });
-            users.Add(new UserLeaderboard { Name = "Cristina", Points = 15017, Rank = 2 });
-            users.Add(new UserLeaderboard { Name = "Lorena", Points = 15017, Rank = 3 });
-            users.Add(new UserLeaderboard { Name = "Cruella", Points = 15017, Rank = 4 });
-            users.Add(new UserLeaderboard { Name = "Tere", Points = 15017, Rank = 5 });
+            //users.Add(new UserLeaderboard { Name = "Valeria", Points = 15017, Rank = 1 });
+            //users.Add(new UserLeaderboard { Name = "Cristina", Points = 15017, Rank = 2 });
+            //users.Add(new UserLeaderboard { Name = "Lorena", Points = 15017, Rank = 3 });
+            //users.Add(new UserLeaderboard { Name = "Cruella", Points = 15017, Rank = 4 });
+            //users.Add(new UserLeaderboard { Name = "Tere", Points = 15017, Rank = 5 });
+
+            string connectionString = "Server=127.0.0.1;Port=3306;Database=bitacora;Uid=root;password=rootroot;";
+
+            MySqlConnection conexion = new MySqlConnection(connectionString);
+            conexion.Open();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conexion;
+            cmd.CommandText = "SELECT usuario.Usuario_ID, usuario.Username, juego.Puntaje_Total FROM juego JOIN usuario ON juego.Usuario = usuario.Usuario_ID ORDER BY Puntaje_Total DESC LIMIT 10;";
+            using (var reader = cmd.ExecuteReader()) 
+            {
+                while (reader.Read())
+                {
+                    UserLeaderboard usr1 = new UserLeaderboard();
+                    usr1.ID = Convert.ToInt32(reader["Usuario_ID"]);
+                    usr1.nombreUsuario = reader["Username"].ToString() ?? "NULL";
+                    usr1.puntaje = Convert.ToInt32(reader["Puntaje_Total"]);
+                    ListaLeader.Add(usr1);
+                }
+            }
+            conexion.Dispose();
 
             TableHTML = @"<table class=""table table-hover""><thead class=""head""><tr><th scope = ""col""> Lugar </ th><th scope=""col"">Inspector</th><th scope = ""col""> Puntos </th ></ tr></ thead><tbody>";
 
             // Tabla a manita
-            foreach(UserLeaderboard usr in users){
-                TableHTML += $"<tr><td scope=\"row\">{usr.Rank}</td><td> <a href=\"Home?UserName={usr.Name}\">{usr.Name}</a> </td ><td> {usr.Points} </td ></tr> ";
+            foreach(UserLeaderboard usr in ListaLeader){
+                TableHTML += $"<tr><td scope=\"row\">{usr.ID}</td><td> <a href=\"Home?UserName={usr.nombreUsuario ?? "N/A"}\">{usr.nombreUsuario ?? "N/A"}</a> </td ><td> {usr.puntaje} </td ></tr> ";
             }
 
             TableHTML += @"</tbody></table>";
@@ -50,6 +77,11 @@ namespace Ternium.Controllers
                 TableHTML = TableHTML
             };
             return View(tmp);
+        }
+
+        public void OnGet()
+        {
+            ListaLeader = new List<UserLeaderboard>();
         }
     }
 }
---
+++ Ternium/Controllers/LeaderboardExController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ternium.Controllers
+{
+    public class LeaderboardExController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
